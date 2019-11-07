eebcef5e2c32b245f943e8c34b73ac962b105665
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-11-07 23:29:54 +0200

    add product desctop

--- js/script.js
+++ js/script.js
@@ -41,6 +41,16 @@ $(document).ready(function () {
         }
     });
 
+    $('.product-tab').not('.product-tab-active').click(function () {
+        var index = $(this).index();
+        var content = $('.tabs').eq(index);
+        $(this).addClass('product-tab-active').siblings().removeClass('product-tab-active');
+        $('.tabs').css('display', 'none').eq(index).css('display', 'block');
+    });
+
+    $('.product-tab:first').addClass('product-tab-active');
+    $('.tabs:first').css('display', 'block');
+
     // document.addEventListener('contextmenu', event => event.preventDefault());
 
     // document.onkeypress = function (event) {
